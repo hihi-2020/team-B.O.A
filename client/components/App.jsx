8a75541d5d27fe8fcb1443abf8aaab0742cd70a0
matt-blackbourn <matt.blackie114@gmail.com>
2020-11-06 12:01:49 +1300

    snake working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,126 @@
 import React from 'react'
-import {Link, Route} from 'react-router-dom'
+import Snake from './Snake'
+
+// import Mouse from './Mouse'
 
 
-const App = () => {
-  return (
-    <>
-      
-    </>
-  )
+class App extends React.Component {
+
+   state = {
+      snakeDots: [
+         [20,40],
+         [40,40],
+         [60,40]
+      ],
+      direction: 'RIGHT',
+      snakeButton: 'Start the Snake!',
+      snakeMoving: false,
+      colour: 'black'
+   }
+
+   componentDidMount() {
+      document.onkeydown = this.onkeydown
+   }
+
+   componentDidUpdate() {
+      this.checkIfOffScreen()
+   }
+
+   onkeydown = (e) => {
+      e = e || window.event
+      switch (e.keyCode) {
+         case 38:
+           this.setState({direction: 'UP'})
+           break
+         case 40:
+           this.setState({direction: 'DOWN'})
+           break
+         case 37:
+           this.setState({direction: 'LEFT'})
+           break
+         case 39:
+           this.setState({direction: 'RIGHT'})
+           break
+       }
+   }
+
+   moveSnake = () => {
+      let dots = [...this.state.snakeDots]
+      let head = dots[dots.length -1]
+
+      switch (this.state.direction) {
+         case 'RIGHT': 
+            head = [head[0] + 20, head[1]]
+            break
+         case 'LEFT':
+            head = [head[0] -20, head[1]]
+            break
+         case 'UP':
+            head = [head[0], head[1] - 20]
+            break
+         case 'DOWN':
+            head = [head[0], head[1] + 20]
+            break
+      }
+      dots.push(head)
+      dots.shift()
+      this.setState({
+         snakeDots: dots
+      })
+   }
+
+   toggleSnakeMovement = () => {
+      if(!this.state.snakeMoving){
+         let move = setInterval(this.moveSnake, 200)
+         this.setState({
+            snakeButton: 'Stop the Snake!',
+            snakeMoving: true
+         })
+      } else {
+         clearInterval(move)
+         this.setState({
+            snakeButton: 'Start the Snake!',
+            snakeMoving: false
+         })
+      }
+   }
+
+   checkIfOffScreen = () => {
+      let head = this.state.snakeDots[this.state.snakeDots.length - 1]
+      // if(head[0] >= window.innerWidth){
+         // this.setState({
+         //    direction: 'DOWN'
+         // })
+      // }
+   }
+
+   pickColour = () => {
+      let colourOptions = ['red', 'green', 'yellow', 'orange', 'aqua', 'blueviolet', 'brown', 'gold', 'greenyellow']
+      let index = Math.floor(Math.random() * colourOptions.length)
+      return colourOptions[index]
+   }
+
+   growSnake = () => {
+      let dots = [...this.state.snakeDots]
+      let tail = dots[0]
+      dots.unshift(tail)
+
+      this.setState({
+         snakeDots: dots,
+         colour: this.pickColour()
+      })
+   }
+
+   render() {
+      return (
+          <div>
+             <button onClick={this.toggleSnakeMovement}>{this.state.snakeButton}</button>
+             {/* <button>Go faster!</button> */}
+             <button onClick={this.growSnake}>Grow Snake</button>
+             <Snake snakeDots={this.state.snakeDots} colour={this.state.colour}/>
+          </div>
+      )
+   }
 }
 
 export default App
