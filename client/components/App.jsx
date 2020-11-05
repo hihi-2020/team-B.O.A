8430b046ffdcda58e0be78149d2d6e01361326a5
Alex-Shatskiy <alexlev706@gmail.com>
2020-11-06 12:16:53 +1300

    Working Progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {Link, Route} from 'react-router-dom'
-import Sound from '../components/Alex'
+import Sound from './Sound'
 
-const App = () => {
+class App extends React.Component {
+  
+  render(){
+    
   return (
     <>
       <Sound/>
     </>
   )
 }
-
+}
 export default App
--- client/components/Sound.jsx
+++ client/components/Sound.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import {Link, Route} from 'react-router-dom'
 import{Howler,Howl} from 'howler'
-// import coin from '../../public/images/coin.jpg'
-
-
+import ReactImage from 'react-image-mapper';
 
 const audio ={
     count: 0,
@@ -14,7 +12,25 @@ const img ={
     src: '../images/coin.png'
 }
 
+
+const randomStyle ={
+    left: 0,
+    right: 500,
+    top: 700,
+    bottom: 600
+}
+
+
 class Sound  extends React.Component  {
+
+
+    
+    randomPosition = () =>{
+        return Math.floor(Math.random() * (700 - 100))
+    }
+    changePosition =()=>{
+
+    }
     
     playSound = (src) =>{
         const sound = new Howl({
@@ -24,18 +40,18 @@ class Sound  extends React.Component  {
         sound.play()
     }
 
-    renderSound =()=>{
-        return(
-        <img src={img.src} onClick={() =>{
-            this.playSound(audio.src)}}
-        
-        />
-        )
-    }
+    // renderSound =()=>{
+    //     return(
+  
+    //     )
+    // }
     render(){
+        console.log(this.randomPosition())
+          
         Howler.volume(1.0)
         return (
         <>
+        <img className="random" style={randomStyle} src={img.src} onClick={() =>{this.playSound(audio.src)}}/>
         {this.renderSound()}
         </>
     )
