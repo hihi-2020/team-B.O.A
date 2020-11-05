0ac3213b0f9b809871fb899786e715464a20e40a
student <student@pop-os.localdomain>
2020-11-06 10:02:50 +1300

    setup header

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Link, Route} from 'react-router-dom'
+import Page_Head from "./Page_Head"
+import Blackie from "./Blackie"
+import Alex from "./Alex"
+import Oli from "./Oli"
 
 
 const App = () => {
   return (
     <>
-      
+      <Page_Head/>
     </>
   )
 }
---
+++ client/components/Page_Head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Page_Head = () => {
+  return (
+    <div class="page-header">
+        <h1>TITLE!</h1>
+    </div>
+  )
+}
+
+export default Page_Head
